703d5182317f222e208f9646f6aaf432a4d7c3c0
Balogh Szilard <105720992+BghSzilard@users.noreply.github.com>
2023-04-26 18:09:46 +0300

    Created project

---
+++ LaboratorAPI-lab-7/DataLayer/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                    .UseSqlServer("Server=LAPTOP-GLKGU87O\\SQLEXPRESS;Database=NewDatabase;User=Szilard; Password=123456;TrustServerCertificate=true", b => b.MigrationsAssembly("Project"))
+                    .LogTo(Console.WriteLine);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Class>().Property(e => e.Name).HasMaxLength(10);
+        }
+
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
