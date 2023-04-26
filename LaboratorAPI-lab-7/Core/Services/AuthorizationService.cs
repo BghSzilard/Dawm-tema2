6adfcb6fd05105b15ed97641544990280986596c
Balogh Szilard <105720992+BghSzilard@users.noreply.github.com>
2023-04-26 19:03:38 +0300

    Finished implementing AuthorizationService class

--- LaboratorAPI-lab-7/Core/Services/AuthorizationService.cs
+++ LaboratorAPI-lab-7/Core/Services/AuthorizationService.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -42,7 +43,101 @@ namespace Core.Services
             var tokenString = jwtTokenHandler.WriteToken(token);
 
             return tokenString;
+        }
+        public bool ValidateToken(string tokenString)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_securityKey));
 
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                IssuerSigningKey = key,
+                ValidateLifetime = true,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = true,
+            };
+
+            if (!jwtTokenHandler.CanReadToken(tokenString.Replace("Bearer ", string.Empty)))
+            {
+                Console.WriteLine("Invalid Token");
+                return false;
+            }
+
+            jwtTokenHandler.ValidateToken(tokenString, tokenValidationParameters, out var validatedToken);
+            return validatedToken != null;
+        }
+
+        public string HashPassword(string password)
+        {
+            if (password == null)
+            {
+                throw new ArgumentNullException("PASSWORD_CANNOT_BE_NULL");
+            }
+
+            byte[] salt;
+            byte[] subkey;
+            using (var deriveBytes = new Rfc2898DeriveBytes(password, SaltSize, PBKDF2IterCount))
+            {
+                salt = deriveBytes.Salt;
+                subkey = deriveBytes.GetBytes(PBKDF2SubkeyLength);
+            }
+
+            var outputBytes = new byte[1 + SaltSize + PBKDF2SubkeyLength];
+            Buffer.BlockCopy(salt, 0, outputBytes, 1, SaltSize);
+            Buffer.BlockCopy(subkey, 0, outputBytes, 1 + SaltSize, PBKDF2SubkeyLength);
+            return Convert.ToBase64String(outputBytes);
+        }
+
+        public bool VerifyHashedPassword(string hashedPassword, string password)
+        {
+            if (hashedPassword == null)
+            {
+                return false;
+            }
+            if (password == null)
+            {
+                throw new ArgumentNullException(nameof(password));
+            }
+
+            var hashedPasswordBytes = Convert.FromBase64String(hashedPassword);
+
+            if (hashedPasswordBytes.Length != (1 + SaltSize + PBKDF2SubkeyLength) || hashedPasswordBytes[0] != 0x00)
+            {
+                return false;
+            }
+
+            var salt = new byte[SaltSize];
+            Buffer.BlockCopy(hashedPasswordBytes, 1, salt, 0, SaltSize);
+            var storedSubkey = new byte[PBKDF2SubkeyLength];
+            Buffer.BlockCopy(hashedPasswordBytes, 1 + SaltSize, storedSubkey, 0, PBKDF2SubkeyLength);
+
+            byte[] generatedSubkey;
+            using (var deriveBytes = new Rfc2898DeriveBytes(password, salt, PBKDF2IterCount))
+            {
+                generatedSubkey = deriveBytes.GetBytes(PBKDF2SubkeyLength);
+            }
+            return ByteArraysEqual(storedSubkey, generatedSubkey);
+        }
+
+        private bool ByteArraysEqual(byte[] a, byte[] b)
+        {
+            if (ReferenceEquals(a, b))
+            {
+                return true;
+            }
+
+            if (a == null || b == null || a.Length != b.Length)
+            {
+                return false;
+            }
+
+            var areSame = true;
+            for (var i = 0; i < a.Length; i++)
+            {
+                areSame &= (a[i] == b[i]);
+            }
+            return areSame;
         }
     }
 }
